e2c4942a7b3f52ed0dbd316f65a1bd1b4f6a57d4
Emanuele <emanuelesquillante@libero.it>
2024-04-08 23:09:24 +0200

    correzioni DOM

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,10 +1,27 @@
-let numerobase = document.getElementById("display");
-let add = document.getElementById("add");
-let dec = document.getElementById("dec");
 let res = document.getElementById("res");
 let cn = document.getElementById("counterName");
 let chn = document.getElementById("changeNameButton");
 let setStarter = document.getElementById("setStart");
+
+let divDisplay = document.getElementById("divDisplay");
+let numerobase = document.createElement ("p");
+numerobase.textContent=("0");
+divDisplay.appendChild (numerobase);
+numerobase.setAttribute("class","display")
+
+let sommatore = document.getElementById ("sommatore");
+let add = document.createElement ("button");
+add.textContent=("+");
+sommatore.appendChild(add);
+add.setAttribute("class", "Add")
+
+let sottrattore = document.getElementById ("sottrattore");
+let dec = document.createElement ("button");
+dec.textContent=("-");
+sottrattore.appendChild(dec);
+dec.setAttribute("class", "Dec");
+
+
 
 add.onclick = function add() {
     numerobase.innerHTML = parseInt(numerobase.innerHTML) + 1;
@@ -26,7 +43,6 @@ chn.onclick = function () {
     cn.innerHTML = newName;
     }
 }
-
 
 setStarter.onclick = function () {
     newNumber = prompt("Enter the number from which to start counting");
